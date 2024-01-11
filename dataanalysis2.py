88a9d69920f1d1555ab8aa7bf5acf9b52b2da62d
sarahrhemadayal <80999949+sarahrhemadayal@users.noreply.github.com>
2024-01-12 00:02:38 +0530

    lect 4 weirdness

---
+++ dataanalysis2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import quandl
+
+api_key = open('quandlapikey.txt', 'r').read()
+df = quandl.get('FMAC/HPI_AK', authtoken=api_key)
+#print(df.head())
+
+fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+
+#print(fiddy_states[0])
+
+df = pd.DataFrame(fiddy_states[0])
+for abbv in df.iloc[0:, 1]:
+    print("FMAC_HPI_"+abbv)
