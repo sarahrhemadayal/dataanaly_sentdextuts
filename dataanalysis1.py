e6d2f5e5878b8322a92bbb6635f70b85c212e3e0
sarahrhemadayal <80999949+sarahrhemadayal@users.noreply.github.com>
2024-01-11 22:43:57 +0530

    vid 3

--- dataanalysis1.py
+++ dataanalysis1.py
@@ -1,4 +1,23 @@
 import pandas as pd
+import quandl
+import numpy as np
 
-df = pd.read_csv('ZILL-Z77006_3B.csv')
+df = pd.read_csv(r'C:\Users\rhema\Downloads\ZILLOW-DATA.csv')
+print(df.head())
+
+df.set_index('date', inplace=True)
+df.to_csv('new0.csv')
+df = pd.read_csv('new0.csv', index_col=0)
+print(df.head())
+
+df = pd.DataFrame(df['value'])
+df.columns=['HPI']
+print(df.head())
+df.to_csv('new1.csv')
+
+df.to_csv('new2.csv', header=False)
+df = pd.read_csv('new2.csv')
+df = pd.read_csv('new2.csv', names=['Dates', '77006_HPI'], index_col=0)
+df.to_csv('new3.csv')
+df = pd.read_csv('new3.csv')
 print(df.head())
