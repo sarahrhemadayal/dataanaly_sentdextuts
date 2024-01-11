0fb030c07af596dbfca3e11b4af3cc74d4bccb17
sarahrhemadayal <80999949+sarahrhemadayal@users.noreply.github.com>
2024-01-12 00:03:42 +0530

    added comments removed some

--- dataanalysis2.py
+++ dataanalysis2.py
@@ -4,11 +4,10 @@ import quandl
 
 api_key = open('quandlapikey.txt', 'r').read()
 df = quandl.get('FMAC/HPI_AK', authtoken=api_key)
-#print(df.head())
+print(df.head())
 
 fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
-
-#print(fiddy_states[0])
+print(fiddy_states[0]) #this is a dataset
 
 df = pd.DataFrame(fiddy_states[0])
 for abbv in df.iloc[0:, 1]:
